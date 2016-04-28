673e0a594a041b7bdf642010ad0491afd0022c0f
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-04-28 19:46:45 +0530

    UpdateDom added to selection

--- code_edit/built-codeEdit.js
+++ code_edit/built-codeEdit.js
@@ -39,9 +39,6 @@ var requirejs, require, define;
      * @returns {String} normalized name
      */
 
-     function alertTest(){
-        alert("I am an alert box!");
-     }
     function normalize(name, baseName) {
         var nameParts, nameSegment, mapValue, foundMap,
             foundI, foundStarMap, starI, i, j, part,
@@ -27871,6 +27868,20 @@ define("orion/editor/editor", [ //$NON-NLS-0$
 					if (tooltip) { tooltip.hide(); }
 					that._updateCursorStatus();
 					that._highlightCurrentLine(e.newValue, e.oldValue);
+
+
+                    var viewDiv = this._viewDiv;
+                    var cursorDiv = util.createElement(viewDiv.ownerDocument, "div"); //$NON-NLS-1$
+                    cursorDiv.className = "textviewBlockCursor"; //$NON-NLS-1$
+                    this._cursorDiv = cursorDiv;
+                    cursorDiv.tabIndex = -1;
+                    cursorDiv.style.zIndex = "2"; //$NON-NLS-1$
+                    cursorDiv.style.color = "transparent"; //$NON-NLS-1$
+                    cursorDiv.style.position = "absolute"; //$NON-NLS-1$
+                    cursorDiv.style.pointerEvents = "none"; //$NON-NLS-1$
+                    cursorDiv.innerHTML = "&nbsp;"; //$NON-NLS-1$
+                    viewDiv.appendChild(cursorDiv);
+                    this._updateDOMSelection();
 				}
 			};
 			textView.addEventListener("ModelChanged", this._listener.onModelChanged); //$NON-NLS-0$
